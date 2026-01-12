10cd3f7fd19b1308d75a38a25851e2561e644506
Fernando Ruan <fernandosingaporefuture@gmail.com>
2026-01-11 21:24:20 -0300

    4A and 69A solved

--- CodeForces/official/69A.c
+++ CodeForces/official/69A.c
@@ -51,43 +51,67 @@ static int number_conversor(const char *str, bool *err) {
 		--positions;
 	}
 	return ;
-}
-
-static bool check_numbers(char *argv) {
-	while (*argv) {
-		int hasNumber = false;
-		if (*argv >= '0' && *argv <= '9' && !hasNumber)
-			hasNumber = true;
-		if (!hasNumber && *(argv + 1) == '\0')
-			return (false);
-		if ((*argv < '0' || *argv > '9') && *argv != '-' && *argv != '+')
-			return (false);
-		++argv;
-	}
-	return (true);
 }*/
 
-static int	extractor_of_numbers(char *str, bool *err) {
+static bool check_numbers(char *str) {
+	bool hasNumber = false;
+	while (*str) {
+		if (*str >= '0' && *str <= '9' && !hasNumber)
+			hasNumber = true;
+		else if ((*str < '0' || *str > '9') && (*str != '\n'
+			&& *str != ' ' && *str != '-' && *str != '+' && *str != '\0'))
+			return (false);
+		++str;
+	}
+	if (!hasNumber && *str == '\0') 
+		return (false);
+	return (true);
+}
+
+static bool	global_checker(int n, int counter) {
+	static int x = 0;
+	static int y = 0;
+	static int z = 0;
+
+	if (counter == 1)
+		x += n;
+	else if (counter == 2)
+		y += n;
+	else if (counter == 3)
+		z += n;
+	else
+		if (x == 0 && y == 0 && z == 0)
+			return (true);
+	return (false);
+}
+
+static void	extractor_of_numbers(char *str, bool *err) {
 	char	buffer[102];
 	int	extracted = 0;
-	int	official = 0;
 	int	index = 0;
 	int	check = 0;
+	int	count = 0;
+
+	if (!check_numbers(str)) {
+		*err = true;
+		return ;
+	}
 
 	while (*str) {
 		if (index == 102) {
 			*err = true;
-			return (0);
+			return ;
 		}
 		else if (*str == ' ' || *str == '\0' || *str == '\n') {
+			++count;
 			buffer[index + 1] = '\0';
 			extracted += number_conversor(buffer, err);
 			if (*err) 
-				return (0);
+				return ;
+			global_checker(extracted, count);
 			check = sizeof(buffer);
 			while (check--)
 				buffer[check] = '\0';
-			official += extracted;
 			extracted = 0;
 			index = 0;
 		}
@@ -98,11 +122,14 @@ static int	extractor_of_numbers(char *str, bool *err) {
 		}
 		++str;
 	}
-	return (official);
+	if (count != 3) {
+		*err = true;
+		return ;
+	}
+	return ;
 }
 
 int main(void) {
-	int result = 0;
 	char	tmp[MAX_LEN];
 
 	// Numbers are dangerous in C language, so I made a function to limitate and avoid overflow and underflow
@@ -125,18 +152,20 @@ int main(void) {
 		while (check--)
 			tmp[check] = '\0';
 		fgets(tmp, MAX_LEN, stdin);
-		if (tmp[0] == '\0') 
+		if (tmp[0] == '\n' || tmp[0] == '\n') {
+			//write(2, "Invalid input\n", 13); 
 			return (0);
-		result += extractor_of_numbers(tmp, &err);
+		}
+		extractor_of_numbers(tmp, &err);
 		if (err) {
 			//write(2, "Error\n", 6);
 			return (0);
 		}
 	}
 
-	if (result == 0)
+	if (global_checker(0, 4))
 		write(1, "YES\n", 4);
 	else 
-		write(2, "NO\n", 3);
+		write(1, "NO\n", 3);
 	return (0);
 }
