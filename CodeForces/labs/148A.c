51108fcb486c38f94339631c034189f600b0028e
Fernando Ruan <fernandosingaporefuture@gmail.com>
2026-01-22 13:55:48 -0300

    339A solved

---
+++ CodeForces/labs/148A.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void) {
+	int k;
+	int l;
+	int m;
+	int n;
+	int d;
+
+	scanf("%d %d %d %d %d", &k, &l, &m, &n, &d);
+
+	//Test multiple scanf
+	//printf("%d %d %d %d %d\n", k, l, m , n, d);
+
+	return (0);
+}
