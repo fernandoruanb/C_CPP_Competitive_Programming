9915c402ceb4ff1f31ff69060f28065eb9f30d53
Fernando Ruan <fernandosingaporefuture@gmail.com>
2026-01-24 10:39:12 -0300

    230A solved and I started mastering each concept of C, CPP and C#

--- CodeForces/labs/A/230A.c
+++ CodeForces/labs/A/230A.c
@@ -1,157 +1,74 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-typedef struct s_phases {
-	int dragonStrength;
-	int bonusWinBattle;
-	t_list *next;
-} t_list;
-
-static t_list *createNewList(void) {
-	t_list *new_list;
-
-	new_list = calloc(1, sizeof(t_list));
-	if (!new_list)
-		return (NULL);
-	new_list->dragonStrength = -1;
-	new_list->bonusWinBattle = -1;
-	new_list->next = NULL;
-	return (new_list);
-}
-
-static void addNewListInFinal(t_list **list, t_list *new) {
-
-	if (!list || !new || !(*list))
-		return ;
-	while ((*list)->next)
-		*list = list->next;
-	*list = new;
-}
-
-static void addNewListInStart(t_list **list, t_list *new) {
-	if (list && new) {
-		new->next = *list;
-		*lst = new;
-	}
-}
-
-static void clearTheListOfPhases(t_list **list) {
-	t_list *next;
-	t_list *tmp;
-
-	while (list) {
-		tmp = *list;
-		while (tmp) {
-			next = tmp->next;
-			free(tmp);
-			tmp = next;
-		}
-		*list = NULL;
-	}
-}
-
-static t_list getTheLastList(t_list *list) {
-	if (!list)
-		return (NULL);
-	while (list->next)
-		list = list->next;
-	return (list);
-}
-
-static void putInOrderList(t_list **list, t_list *new) {
-	if (!list || !new)
-		return ;
-	if (!(*list)) {
-		*list = new;
-		return ;
-	}
-	while ((*list)->next) {
-		if ((*list)->dragonStrength <= new->dragonStrength) {
-			(*list)->next = new;
-			return ;	
-		} else if ((*list)->dragonStrength > new->dragonStrength){
-			new->next = *list;
-			*list = new;
-			return ;
-		}
-		(*list) = (*list)->next;
-	}
-}
-
-static char checkResult(int kiritoStrength, t_list *list) {
-	if (!list)
-		return ('\0');
-
-	char result = '\0';
-
-	while (list->next) {
-		if (kiritoStrength >= list->dragonStrength) {
-			kiritoStrength += list->bonusWinBattle;
-			result = 'Y';
-		} else {
-			result = 'N';
-			break ;
-		}
-		list = list->next;
-	}
-	return (result);
-}
+/*
+        Author: Fernando Ruan
+        Profile: https://codeforces.com/profile/fruan-ba
+        Country: Brazil
+        ChallengeLink: https://codeforces.com/problemset/problem/230/A
+        I am happy to code :)
+*/
 
 int main(void) {
 	int *room = (int *)calloc(2, sizeof(int));
 
-	if (!room)
-		return (0);
-
 	for (int i = 0; i < 2; ++i)
 		scanf("%d", room + i);
 
-	if (room[0] < 0 || room[0] > 10000 || room[1] < 0 || room[1] > 1000) {
+	if (room[0] < 1 || room[0] > 10000 || room[1] < 1 || room[1] > 1000) {
 		free(room);
 		return (0);
 	}
 
-	int phases = room[1];
-	char result = '\0';
+	int *dragonStrength = (int *)calloc(room[1], sizeof(int));
+
+	if (!dragonStrength) {
+		free(room);
+		return (0);
+	}
+
+	int *bonusWinBattle = (int *)calloc(room[1], sizeof(int));
+
+	if (!bonusWinBattle) {
+		free(room);
+		free(dragonStrength);
+		return (0);
+	}
+
 	int kiritoStrength = room[0];
-	int maxDragonStrength = 0;
-	int *phase = NULL;
-	t_list *new_phase = createNewList();
+	int dragons = 0;
+	int bonus = 0;
 
-	if (!new_phase) {
-		free(room);
-		return (0);
+	for (int i = 0; i < room[1]; ++i) {
+		scanf("%d", dragonStrength + dragons);
+		++dragons;
+		scanf("%d", bonusWinBattle + bonus);
+		++bonus;
 	}
-
-	while (phases--) {
-		phase = (int *)calloc(2, sizeof(int));
-		if (!phase) {
-			free(room);
-			free(phase);
-			return (0);
+	int counter = 0;
+	bool flag = false;
+	for (int i = 0; i < room[1]; ++i) {
+		counter = 0;
+		while (counter < room[1]) {
+			if (kiritoStrength > dragonStrength[counter] && dragonStrength[counter] != -1)
+				break ;
+			++counter;
 		}
-		for (int i = 0; i < 2; ++i)
-			scanf("%d", phase + i);
-
-		t_list *new = createNewList();
-		if (!new) {
-			clearTheListOfPhases(&new_phase);
-			free(room);
-			free(phase);
-			return (0);
-		}
-		new->dragonStrength = phase[0];
-		new->bonusWinBattle = phase[1];
-		putInOrderList(&new_phase, new);
-		free(phase);
-		phase = NULL;
+		if (counter != room[1]) {
+			dragonStrength[counter] = -1;
+			kiritoStrength += bonusWinBattle[counter];
+			flag = true;
+		} else 
+			flag = false;
 	}
-	result = checkResult(kiritoStrength, new_phase);
-	if (result == 'Y')
-		write(1, "YES\n", 4);
-	else if (result == 'N')
-		write(1, "NO\n", 3);
+	free(bonusWinBattle);
+	free(dragonStrength);
 	free(room);
+	if (flag)
+		write(1, "YES\n", 4);
+	else
+		write(1, "NO\n", 3);
 	return (0);
 }
